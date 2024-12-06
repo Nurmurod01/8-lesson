16399a83631cdd46d6d8503c5c92d85110396122
Nurmurod01 <rabbimovnurmurod@gmail.com>
2024-12-06 19:01:19 +0500

    8-darsda

---
+++ src/image/index.js
@@ -0,0 +1,6 @@
+export { default as Logo } from "./Logo.png";
+export { default as HeroImg } from "./Hero.svg";
+export { default as Pointer1 } from "./pointer1.svg";
+export { default as Pointer2 } from "./pointer2.png";
+export { default as Pointer3 } from "./pointer3.png";
+export { default as Pointer4 } from "./pointer4.png";
